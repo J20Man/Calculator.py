cb52a8a89de1c63b647f27f702142031452b2a59
Joe Twentyman <josephtwentyman@hotmail.co.uk>
2025-02-22 19:11:38 +0000

    Added function to add number, only works with single dig at the mo

--- window.py
+++ window.py
@@ -12,30 +12,28 @@ bttns = ttk.Frame(root, padding=10)
 #defines the buttons needed for the calculator
 bttns.grid()
 display = tk.Label(bttns, text="0", bg="grey").grid(column=0, row=0, columnspan=5)
-# ttk.Label(symbols, text="Hello World!").grid(column=0, row=0)
-ttk.Button(bttns, text="+").grid(column=4, row=1)
-ttk.Button(bttns, text="-").grid(column=5, row=1)
-ttk.Button(bttns, text="X").grid(column=4, row=2)
-ttk.Button(bttns, text="/").grid(column=5, row=2)
-ttk.Button(bttns, text="=").grid(column=5, row=3)
 
-def listitems(self, event):
-    print(event.widget['text'])
+run = 0
+total = 0
 
-# #defiens the buttons needed for the numbers
-# ttk.Button(bttns, text="1").bind("<Button-1>", listitems()).grid(column=1, row=3)
-# ttk.Button(bttns, text="2").grid(column=2, row=3)
-# ttk.Button(bttns, text="3").grid(column=3, row=3)
-# ttk.Button(bttns, text="=").grid(column=5, row=3)
-# #row two
-# ttk.Button(bttns, text="4").grid(column=1, row=2)
-# ttk.Button(bttns, text="5").grid(column=2, row=2)
-# ttk.Button(bttns, text="6").grid(column=3, row=2)
-# #row three
-# ttk.Button(bttns, text="7").grid(column=1, row=1)
-# ttk.Button(bttns, text="8").grid(column=2, row=1)
-# ttk.Button(bttns, text="9").grid(column=3, row=1)
 
+def listitems(event):
+    # print(event.widget['text'])
+    global total 
+    num = int(event.widget['text'])
+    total += num 
+    print(total)
+        
+
+symbols = [
+    ('+', 4, 1), ('-', 5, 1), ('X', 4, 2),
+    ('/', 5, 2), ('=', 5, 3)
+]
+
+for num, col, row in symbols:
+    btn = ttk.Button(bttns, text=str(num))
+    btn.grid(column=col, row=row)
+    btn.bind("<Button-1>", listitems)  # Bind the function
 
 # Number Buttons
 numbers = [
