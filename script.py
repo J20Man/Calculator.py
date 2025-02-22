ca7f817d769914a652f26696ac2533102bc960fa
Joe Twentyman <josephtwentyman@hotmail.co.uk>
2025-02-22 17:22:51 +0000

    very simple addition

--- script.py
+++ script.py
@@ -1,1 +1,8 @@
-print("hello world")
+#Takes the users input
+first_input = int(input("Enter your first number "))
+function = input("What would you like to do to your number")
+second_input = int(input("What would you like to by?"))
+
+calc = first_input + second_input
+
+output = print(calc)
